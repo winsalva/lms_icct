f75a17faa8e8149edf5e5b186a4a6072410c432b
* <winsalva396@gmail.com>
2021-11-09 13:33:32 +0800

    1st commit

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :app, App.Repo,
   username: "postgres",
   password: "postgres",
-  database: "app_dev",
+  database: "brokerage",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
